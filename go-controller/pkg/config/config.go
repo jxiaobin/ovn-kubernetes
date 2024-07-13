b86df5a86601b42494d9c81432bd83dc4d425bde
Flavio Fernandes <ffernandes@nvidia.com>
2024-07-13 16:52:59 +0000

    Add flag to enable the feature
    This commit adds a new flag called
    enable-network-qos, which can be used
    to feature gate the Network QoS feature.
    
    The following flag is exposed in kind
    scripts as -nqe or --network-qos-enable:
    
       kind.sh -nqe
       kind-helm.sh -nqe
    
    Alternatively, it can be enabled via an
    exported shell variable:
    
       OVN_NETWORK_QOS_ENABLE=true kind.sh
    
    Signed-off-by: Flavio Fernandes <ffernandes@nvidia.com>

--- go-controller/pkg/config/config.go
+++ go-controller/pkg/config/config.go
@@ -407,6 +407,7 @@ type OVNKubernetesFeatureConfig struct {
 	EnablePersistentIPs             bool `gcfg:"enable-persistent-ips"`
 	EnableDNSNameResolver           bool `gcfg:"enable-dns-name-resolver"`
 	EnableServiceTemplateSupport    bool `gcfg:"enable-svc-template-support"`
+	EnableNetworkQoS                bool `gcfg:"enable-network-qos"`
 }
 
 // GatewayMode holds the node gateway mode
@@ -1068,6 +1069,12 @@ var OVNK8sFeatureFlags = []cli.Flag{
 		Usage:       "Configure to use svc-template with ovn-kubernetes.",
 		Destination: &cliConfig.OVNKubernetesFeature.EnableServiceTemplateSupport,
 		Value:       OVNKubernetesFeature.EnableServiceTemplateSupport,
+	},
+	&cli.BoolFlag{
+		Name:        "enable-network-qos",
+		Usage:       "Configure to use NetworkQoS CRD feature with ovn-kubernetes.",
+		Destination: &cliConfig.OVNKubernetesFeature.EnableNetworkQoS,
+		Value:       OVNKubernetesFeature.EnableNetworkQoS,
 	},
 }
 
